cf9a6edebd4f594788ed766245d581c3d958721b
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-09 22:23:35 +0100

    Implemented sigmoud neuron

--- src/header/neuron.hpp
+++ src/header/neuron.hpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 
 class Neuron {
 public:
     Neuron(double w1, double w2, double bias);
     double sigmoid(double x);
+    double feedForward(double x1, double x2);
 
 private:
     double weights[2];
--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,1 +1,24 @@
 #include "header/neuron.hpp"
+
+
+Neuron::Neuron(double w1, double w2, double bias) 
+{
+    weights[0] = w1;
+    weights[1] = w2;
+    this->bias = bias;
+}
+
+double Neuron::sigmoid(double x)
+{
+    return 1 / (1 + exp(-x));    
+}
+
+double Neuron::feedForward(double x1, double x2)
+{
+    // Calculate the weighted sum of the inputs
+    double weightedSum = x1 * weights[0] + x2 * weights[1] + bias;
+    // Return the result of the sigmoid function
+    double result = sigmoid(weightedSum);
+
+    return result > 0.5 ? 1 : 0;
+}
