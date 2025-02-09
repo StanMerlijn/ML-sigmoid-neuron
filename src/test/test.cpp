f7db39fd3e1fd88dc82b01180837c3949ab652e4
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-09 22:51:41 +0100

    Not gates testing / explenaition bias

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -6,6 +6,11 @@
 TEST_CASE("Neuron AND gate", "[neuron]") {
     // Create a neuron with weights 1, 1 and bias -1.5
     // With a bias of -1.5 the dot product will only be greater than 0 if both inputs are 1
+    // x1 = 0, x2 = 0: 1 * 0 + 1 * 0 - 1.5 = -1.5
+    // x1 = 0, x2 = 1: 1 * 0 + 1 * 1 - 1.5 = -0.5
+    // x1 = 1, x2 = 0: 1 * 1 + 1 * 0 - 1.5 = -0.5
+    // x1 = 1, x2 = 1: 1 * 1 + 1 * 1 - 1.5 = 0.5
+
     std::vector<double> weights = {1, 1};
     Neuron n(weights, -1.5);
     REQUIRE(n.feedForward({0, 0}) == 0);
@@ -17,6 +22,11 @@ TEST_CASE("Neuron AND gate", "[neuron]") {
 TEST_CASE("Neuron OR gate", "[neuron]") {
     // Create a neuron with weights 1, 1 and bias -0.5
     // With a bias of -0.5 the dot product will be greater than 0 if any of the inputs are 1
+    // x1 = 0, x2 = 0: 1 * 0 + 1 * 0 - 0.5 = -0.5
+    // x1 = 0, x2 = 1: 1 * 0 + 1 * 1 - 0.5 = 0.5
+    // x1 = 1, x2 = 0: 1 * 1 + 1 * 0 - 0.5 = 0.5
+    // x1 = 1, x2 = 1: 1 * 1 + 1 * 1 - 0.5 = 1.5
+
     std::vector<double> weights = {1, 1};
     Neuron n(weights, -0.5);
     REQUIRE(n.feedForward({0, 0}) == 0);
@@ -27,10 +37,30 @@ TEST_CASE("Neuron OR gate", "[neuron]") {
 
 TEST_CASE("Neuron NOT gate", "[neuron]") {
     // Create a neuron with weights -2 and 0 and bias 1
+    // With a bias of 1 the dot product will be greater than 0 if the first input is 0
+    // x1 = 1: -2 * 1 + 0 * 1 + 1 = -1
+    // x1 = 0: -2 * 0 + 0 * 0 + 1 = 1
+
     std::vector<double> weights = {-2, 0};
     Neuron n(weights, 1);
     REQUIRE(n.feedForward({0, 0}) == 1);
-    REQUIRE(n.feedForward({0, 1}) == 1);
+    REQUIRE(n.feedForward({0, 0}) == 1);
+    REQUIRE(n.feedForward({1, 0}) == 0);
+    REQUIRE(n.feedForward({1, 0}) == 0);
+}
+
+TEST_CASE("Neuron NOR gate (NOT OR)", "[neuron]") {
+    // Create a neuron with weights -1, -1 and bias 0.5
+    // With a bias of 0.5 the dot product will be greater than 0 if both inputs are 0
+    // x1 = 0, x2 = 0: -1 * 0 + -1 * 0 + 0.5 = 0.5
+    // x1 = 0, x2 = 1: -1 * 0 + -1 * 1 + 0.5 = -0.5
+    // x1 = 1, x2 = 0: -1 * 1 + -1 * 0 + 0.5 = -0.5
+    // x1 = 1, x2 = 1: -1 * 1 + -1 * 1 + 0.5 = -1.5
+    
+    std::vector<double> weights = {-1, -1};
+    Neuron n(weights, 0.5);
+    REQUIRE(n.feedForward({0, 0}) == 1);
+    REQUIRE(n.feedForward({0, 1}) == 0);
     REQUIRE(n.feedForward({1, 0}) == 0);
     REQUIRE(n.feedForward({1, 1}) == 0);
 }
