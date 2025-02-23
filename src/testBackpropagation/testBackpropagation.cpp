edee55eb4e0648c079a04cfca465824fee33d803
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-24 00:00:28 +0100

    Unit test fort neuronLayer

--- src/testBackpropagation/testBackpropagation.cpp
+++ src/testBackpropagation/testBackpropagation.cpp
@@ -12,6 +12,9 @@
 #include "../header/catch.hpp"
 #include "../header/common.hpp"
 #include "../header/csv_reader.hpp"
+#include "../header/neuron.hpp"
+#include "../header/neuronLayer.hpp"
+#include "../header/neuronNetwork.hpp"
 
 /**
  * @brief In this test case we test if we can load the digits dataset.
@@ -24,4 +27,11 @@ TEST_CASE("Loading digit data", "[backpropagation]") {
     // Check the size of the data
     REQUIRE(_digitData.images.size() == 1797 * 64);
     REQUIRE(_digitData.targets.size() == 1797);
+}
+
+TEST_CASE("Testing initialization of the NeuronLayer", "[NeuronLayer]")
+{
+    // create a neuronLayer with 10 neurons
+    NeuronLayer nl(10, 4);
+    nl.__str__();
 }
